8c72efb873a25eb4caad968a1a8ab50d7e387314
minorsevenflatfive <bramanta.n@gmail.com>
2022-11-10 21:33:58 +0700

    revert main to hello world

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
-
-fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
+
